c66c8c1aecb25aff39e140d41807fcfa2279cb6d
Aswin <aswin@zweck.io>
2024-06-11 13:39:15 +0530

    Added gitignore

---
+++ poc-cloud/functions/src/index.ts
@@ -0,0 +1,4 @@
+export {ping} from "./cloud-functions/ping";
+export {subscribe} from "./cloud-functions/subscribe";
+export {unsubscribe} from "./cloud-functions/unsubscribe";
+export {publish} from "./cloud-functions/publish";
